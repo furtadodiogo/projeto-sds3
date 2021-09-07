c4e681e79700876968d48f6b64ee8ad2fc2d0d17
dfurtado <dfurtado@uolinc.com>
2021-09-06 23:47:45 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/furtadodiogo" target="_blank" rel="noreferrer">Diogo Furtado</a></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
