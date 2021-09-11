f7099b401c35136e5de33014f1d5f9af98291a90
dfurtado <dfurtado@uolinc.com>
2021-09-10 22:52:37 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+            <div>
+                <h1>Página Home</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
